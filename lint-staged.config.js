5c451a5761642885e9c9ca1c6c7f2abff92bb8db
Thomas Deinhamer <code@thasmo.dev>
2025-06-20 14:48:44 +0200

    Fix linting issues

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,4 @@
 export default {
-	'default.json': [
-		'prettier --check',
-		'cspell --no-progress --no-must-find-files',
-		'renovate-config-validator',
-	],
-	'**/*.{md,json,yml,yaml,js}': [
-		'prettier --check',
-		'cspell --no-progress --no-must-find-files',
-	],
+	'default.json': ['prettier --check', 'cspell --no-progress --no-must-find-files', 'renovate-config-validator'],
+	'**/*.{md,json,yml,yaml,js}': ['prettier --check', 'cspell --no-progress --no-must-find-files'],
 };
